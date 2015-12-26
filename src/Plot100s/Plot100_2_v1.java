4e29b33b7cb94d8348cef018e275232d76ff59be
Sandeep Sasidharan <sasidhar.sandeep@gmail.com>
2015-12-26 10:49:19 -0600

    100 pool object added

---
+++ src/Plot100s/Plot100_2_v1.java
@@ -0,0 +1,66 @@
+package Plot100s;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.joda.time.LocalDateTime;
+
+import Graph.Pair;
+import Graph.ShareabilityGraph;
+import PlotGenerators.Plot2;
+import Trip.TaxiTrip;
+import Trip.TripLoader;
+
+public class Plot100_2_v1 {
+	public static final Logger LOGGER = Logger.getLogger(Plot2.class.getName());
+
+	public static void main (String[] args0) throws IOException, ClassNotFoundException{
+		PrintStream out = new PrintStream(new FileOutputStream("out_P100_p2_5minWalk_v1.csv"));
+		System.setOut(out);
+		LOGGER.info("Run started at "+ LocalDateTime.now() );
+		ObjectInputStream ios_graph_read = new 
+				ObjectInputStream(new 
+						FileInputStream("ObjectWarehouse/TripData/Random100Pools.obj"));
+		List<List<TaxiTrip>> random_100_pools = new ArrayList<List<TaxiTrip>>();
+		random_100_pools =   (List<List<TaxiTrip>>) ios_graph_read.readObject();
+
+		/////////////     START MERGE TEST   /////////////////////
+		Collections.shuffle(random_100_pools);
+		PrintWriter merge_trips_writer = new PrintWriter(new FileWriter ("Plot100_P2_5minWalk_v1.csv"));
+		String print_date_time = new String();
+		TripLoader tripLoader = new TripLoader();
+		for(List<TaxiTrip> trips:random_100_pools){
+			ShareabilityGraph sG = new ShareabilityGraph();
+			List<Pair<TaxiTrip,TaxiTrip>> mergeable_trips = new ArrayList<Pair<TaxiTrip,TaxiTrip>>();
+			for(int i = 0 ; i < trips.size(); i ++){
+				for(int j = i+1 ; j < trips.size(); j ++){
+					TaxiTrip trip_A = trips.get(i);
+					TaxiTrip trip_B = trips.get(j);
+					print_date_time = trip_A.getPickupDate();
+					if(trip_A.getPassengerCount()+trip_B.getPassengerCount()<=4){
+						if(sG.checkMergeable(trip_A,trip_B,tripLoader,merge_trips_writer)){
+							mergeable_trips.add(new Pair<TaxiTrip,TaxiTrip>(trip_A,trip_B));
+						}
+					}
+				}
+			}
+			sG.constructShareabilityGraph(mergeable_trips);
+			int matches =   sG.findMaxMatch(merge_trips_writer);
+			int red_trips = trips.size() - matches;
+			System.out.println(print_date_time+", "+trips.size()+", "+matches+", "+red_trips);
+
+		}
+		LOGGER.info("Run Ended at "+ LocalDateTime.now() );
+		merge_trips_writer.close();
+		out.close();
+	}
+}
